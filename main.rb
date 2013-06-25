02168b80cc6ecbbf4a6339b6e8191eba8300b69d
Alina Guzman <alinochka8@gmail.com>
2013-06-25 14:31:09 -0400

    Created html view layout with header tags to give instructions on the form, and created a basic layout of the form on quote.erb file

--- main.rb
+++ main.rb
@@ -4,5 +4,9 @@ require 'sinatra/reloader' if development?
 require 'yahoofinance'
 
 get '/' do
+  @symbol = params[:symbol]
+  @result = YahooFinance::get_quotes(YahooFinance::StandardQuote, @symbol)[@symbol].lastTrade
   erb :quote
 end
+
+
