123e4989bc8de8e699d9c862c76e86cf3921d9b6
Zachariah Reitano <zreitano@gmail.com>
2013-06-25 15:02:57 -0400

    Stock Ticker Functioning

--- main.rb
+++ main.rb
@@ -3,6 +3,12 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'yahoofinance'
 
-get '/' do
+get '/stock_lab' do
+ @ticker = params[:ticker]
+  if @ticker
+    @price = YahooFinance::get_quotes(YahooFinance::StandardQuote, @ticker)[@ticker].lastTrade
+  else
+    @price = nil
+  end
   erb :quote
 end
