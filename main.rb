0c97a79900f2636cacf66b7e51aadbee3994f907
Christine Coulter <christinetcoulter@gmail.com>
2013-06-25 14:53:46 -0400

    Set up main.rb with initial code

--- main.rb
+++ main.rb
@@ -4,5 +4,8 @@ require 'sinatra/reloader' if development?
 require 'yahoofinance'
 
 get '/' do
+  @ticker = params[:ticker]
+  @result = YahooFinance::get_quotes(YahooFinance::StandardQuote, @ticker)[@ticker].lastTradereturn
   erb :quote
 end
+
