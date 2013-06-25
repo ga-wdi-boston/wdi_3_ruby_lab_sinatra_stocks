17b2636cc556e021ece85ca537fe6447e1207c68
mvanger <mvanger@nyu.edu>
2013-06-25 14:26:13 -0400

    Added last trade price method

--- main.rb
+++ main.rb
@@ -3,8 +3,8 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'yahoofinance'
 
-get '/quote' do
+get '/quote/:ticker' do
   @ticker = params[:ticker].upcase
-
+  return YahooFinance::get_quotes(YahooFinance::StandardQuote, @ticker)[@ticker].lastTrade.to_s
   erb :quote
 end
