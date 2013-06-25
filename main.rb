3f5ad945de0166c5db5690680180e0991a9aaf00
Kevon Cheung <kevon.cheung@gmail.com>
2013-06-25 15:24:15 -0400

    done

--- main.rb
+++ main.rb
@@ -3,6 +3,13 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'yahoofinance'
 
-get '/' do
+get '/get_price' do
+  @ticker = params[:ticker]
+
+  # nothing to load when the page first loads
+  if @ticker
+    @price = YahooFinance::get_quotes(YahooFinance::StandardQuote, @ticker)[@ticker].lastTrade
+  end
+
   erb :quote
 end
