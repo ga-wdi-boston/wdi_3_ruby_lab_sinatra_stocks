e0a7c97bd0242983283909a6941d676565e4f6d7
Jamal Powell <jamalkpowell@gmail.com>
2013-06-25 15:15:19 -0400

    added a stock calculator

--- main.rb
+++ main.rb
@@ -3,6 +3,14 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'yahoofinance'
 
-get '/' do
+# get '/' do
+#   erb :quote
+# end
+
+get '/quote' do
+  @ticker = params[:ticker]
+if @ticker
+  @price = YahooFinance::get_quotes(YahooFinance::StandardQuote, @ticker)[@ticker].lastTrade.round(2)
+  end
   erb :quote
 end
