df24e45940d674ff48c4f3142a6c21e7ea85710c
Christine Coulter <christinetcoulter@gmail.com>
2013-06-25 15:01:36 -0400

    add field and button to quote.erb

--- main.rb
+++ main.rb
@@ -4,8 +4,8 @@ require 'sinatra/reloader' if development?
 require 'yahoofinance'
 
 get '/' do
-  @ticker = params[:ticker]
-  @result = YahooFinance::get_quotes(YahooFinance::StandardQuote, @ticker)[@ticker].lastTradereturn
+  # @ticker = params[:ticker]
+  # @result = YahooFinance::get_quotes(YahooFinance::StandardQuote, @ticker)[@ticker].lastTradereturn
   erb :quote
 end
 
