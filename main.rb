fd2d05375138c548d48f52a870dfffbb51ba4db3
Ben Israeli <jisraeli@gmail.com>
2013-06-25 15:18:32 -0400

    added h1, layout, quote with help from phil

--- main.rb
+++ main.rb
@@ -1,8 +1,19 @@
 require 'pry'
+require 'yahoofinance'
 require 'sinatra'
 require 'sinatra/reloader' if development?
-require 'yahoofinance'
 
 get '/' do
+  if params.empty?
+    @stock = nil
+  else
+    @ticker = params[:stock_quote]
+    @stock = YahooFinance::get_quotes(YahooFinance::StandardQuotes, @ticker)[@ticker].lastTrade
+  end
   erb :quote
+
+end
+
+get '/about' do
+  erb :about
 end
