4152e888d84c5a6bdfaa4891c3ca29f82e238192
Wesley Carr <surfswell17@gmail.com>
2013-06-25 15:04:29 -0400

    Set @market_value equal to @ticker if @ticker was not present during form deployment

--- main.rb
+++ main.rb
@@ -3,6 +3,13 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'yahoofinance'
 
-get '/' do
-  erb :quote
+get '/fetch_stock' do
+  #name of the hash is always params[:___]
+  @ticker = params[:ticker]
+  if @ticker
+    @market_value = YahooFinance::get_quotes(YahooFinance::StandardQuote, @ticker)[@ticker].lastTrade
+  else
+    @market_value = nil
+  end
+    erb (:quote)
 end
