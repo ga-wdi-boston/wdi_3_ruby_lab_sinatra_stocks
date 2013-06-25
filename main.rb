57379b9f32d5872d47a052ee02f24c655d776a1f
Anuj Jhunjhunwala <anuj.jhunjhunwala@gmail.com>
2013-06-25 15:04:42 -0400

    Built out files, quote functionality works

--- main.rb
+++ main.rb
@@ -3,6 +3,13 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'yahoofinance'
 
-get '/' do
+get '/quote' do
+  @ticker = params[:ticker]
+  if @ticker
+    @quote = YahooFinance::get_quotes(YahooFinance::StandardQuote, @ticker)[@ticker].lastTrade
+  else
+    @quote = nil
+  end
   erb :quote
 end
+
