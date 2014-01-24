ef3a24cbab54113ac6e5ddfc61118103fd52da3b
Jordan Morano <jordan.morano@gmail.com>
2014-01-24 14:13:18 -0500

    css clean up

--- main.rb
+++ main.rb
@@ -11,19 +11,22 @@ get '/' do
   params.keys.map { |e| e != :ticker && params[e.to_sym] == 'on' ? request << e.to_sym : nil } if params.keys
   request.flatten!
   ticker = ticker.upcase if ticker
-  data = YahooFinance.quotes([ticker], request )
-  data.each do |stock|
-    @name = stock.name
-    @sym = stock.symbol
-    @last = stock.last_trade_price.to_f.round(2)
-    @close = stock.close.to_f.round(2)
-    @open = stock.open.to_f.round(2)
-    @volume = stock.volume
-    @avg_daily_vol = stock.average_daily_volume
-    @bid = stock.bid
-    @mark_cap = stock.market_capitalization
-    @float = stock.float_shares
-    @exchange = stock.stock_exchange
+  if ticker
+    data = YahooFinance.quotes([ticker], request )
+    data.each do |stock|
+      @name = stock.name
+      @sym = stock.symbol
+      @last = stock.last_trade_price.to_f.round(2)
+      @close = stock.close.to_f.round(2)
+      @open = stock.open.to_f.round(2)
+      @volume = stock.volume
+      @avg_daily_vol = stock.average_daily_volume
+      @bid = stock.bid
+      @mark_cap = stock.market_capitalization
+      @exchange = stock.stock_exchange
+    end
+  else
+    @name = '<span>Input a stock!</span>'
   end
   erb :quote
 end
