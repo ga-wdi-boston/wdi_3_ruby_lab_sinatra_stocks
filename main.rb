aab90a171e832890a6576c9adde64cb2bf8ab90f
David Fisher <tibbon@gmail.com>
2014-01-24 15:08:21 -0500

    Added Twitter

--- main.rb
+++ main.rb
@@ -2,6 +2,21 @@ require 'pry'
 require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'yahoo_finance'
+require 'dotenv'
+require 'twitter'
+
+Dotenv.load
+
+set :server, 'webrick'
+
+def twitter_client
+	client = Twitter::REST::Client.new do |config|
+	  config.consumer_key        = ENV["CONSUMER_KEY"]
+	  config.consumer_secret     = ENV["CONSUMER_SECRET"]
+	  config.access_token        = ENV["ACCESS_TOKEN"]
+	  config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
+	end
+end
 
 get '/' do
   erb :quote
@@ -12,3 +27,10 @@ get '/quote/?:ticker?' do
 	@data = YahooFinance.quotes([@ticker], [:volume, :last_trade_price, :high_52_weeks, :low_52_weeks]).first
 	erb :quote
 end
+
+get '/twitter/:username' do
+	@user = params[:username]
+	@tweets = twitter_client.user_timeline(@user)
+	erb :tweets
+end
+
