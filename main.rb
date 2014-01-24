dfb02a9927c34c59e03263a0cf99f33c383fa30a
Jordan Morano <jordan.morano@gmail.com>
2014-01-24 13:49:05 -0500

    stocks with form

--- main.rb
+++ main.rb
@@ -1,8 +1,29 @@
 require 'pry'
 require 'sinatra'
 require 'sinatra/reloader' if development?
-require 'yahoofinance'
+require 'yahoo_finance'
+
+
 
 get '/' do
+  ticker = params[:ticker]
+  request = [:name, :last_trade_price]
+  params.keys.map { |e| e != :ticker && params[e.to_sym] == 'on' ? request << e.to_sym : nil } if params.keys
+  request.flatten!
+  ticker = ticker.upcase if ticker
+  data = YahooFinance.quotes([ticker], request )
+  data.each do |stock|
+    @name = stock.name
+    @sym = stock.symbol
+    @last = stock.last_trade_price.to_f.round(2)
+    @close = stock.close.to_f.round(2)
+    @open = stock.open.to_f.round(2)
+    @volume = stock.volume
+    @avg_daily_vol = stock.average_daily_volume
+    @bid = stock.bid
+    @mark_cap = stock.market_capitalization
+    @float = stock.float_shares
+    @exchange = stock.stock_exchange
+  end
   erb :quote
 end
