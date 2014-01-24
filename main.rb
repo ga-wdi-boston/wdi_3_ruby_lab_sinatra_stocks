a0172003ec96fbf4962368bff8d9d25e78eae9cc
Mike Stone <mikestone14@gmail.com>
2014-01-24 14:06:56 -0500

    site BEAUTIFICATION

--- main.rb
+++ main.rb
@@ -1,8 +1,26 @@
 require 'pry'
 require 'sinatra'
 require 'sinatra/reloader' if development?
-require 'yahoofinance'
+require 'yahoo_finance'
+
+get '/quote/?:ticker?' do
+	@ticker = params[:ticker]
+	@data = YahooFinance.quotes([@ticker], [:low_52_weeks, :high_52_weeks, :last_trade_price, :pe_ratio, :last_trade_time])
+	@last_trade_price = @data[0].last_trade_price
+	@low_52_weeks = @data[0].low_52_weeks
+	@high_52_weeks = @data[0].high_52_weeks
+	@pe_ratio = @data[0].pe_ratio
+	@last_trade_time = @data[0].last_trade_time
+  erb :quote
+end
 
 get '/' do
+	@ticker = params[:quotes]
+	@data = YahooFinance.quotes([@ticker], [:low_52_weeks, :high_52_weeks, :last_trade_price, :pe_ratio, :last_trade_time])
+	@last_trade_price = @data[0].last_trade_price
+	@low_52_weeks = @data[0].low_52_weeks
+	@high_52_weeks = @data[0].high_52_weeks
+	@pe_ratio = @data[0].pe_ratio
+	@last_trade_time = @data[0].last_trade_time
   erb :quote
 end
